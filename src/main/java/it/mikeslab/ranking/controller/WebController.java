3cd86b4009e5fe9576408bc167e1700c5ac5868b
MichaelAPI <michele@mikeslab.it>
2024-12-10 13:27:45 +0100

    Switching to MVC, further improvements

---
+++ src/main/java/it/mikeslab/ranking/controller/WebController.java
@@ -0,0 +1,27 @@
+package it.mikeslab.ranking.controller;
+
+import it.mikeslab.ranking.entity.Player;
+import it.mikeslab.ranking.repository.PlayerRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    private final PlayerRepository playerRepository;
+
+    public WebController(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @GetMapping("/leaderboard")
+    public String getLeaderboard(Model model) {
+        List<Player> players = playerRepository.findAll();
+        model.addAttribute("players", players);
+        return "index";
+    }
+
+}
